ca6cf08f9f373cdf3c0b253b62088f3751d48439
Benjamin Kammoun <beny.kam@outlook.com>
2024-02-18 16:56:03 +0200

    task-000001--add DMS (API) link to Next configurations.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    publicRuntimeConfig: {
+        DMS: process.env.DMS
+        ? process.env.DMS.replace(/\/?$/, '')
+        : 'https://data.gov.il/',
+    },
+}
 
 module.exports = nextConfig
