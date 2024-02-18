3318759a66c9e8c3cfd64eccc92694529b304670
Benjamin Kammoun <beny.kam@outlook.com>
2024-02-18 17:04:41 +0200

    task-000001--about route page template added.

---
+++ app/about/conditions/page.tsx
@@ -0,0 +1,5 @@
+export default function ConditionsPage() {
+    return (
+        <div>About Conditions</div>
+    );
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { ArticleSkeleton } from "../components/skeletons";
+
+export default function AboutPage(){
+    return (
+        <div>
+            <ArticleSkeleton className="m-8 h-full"/>
+        </div>
+    );
+}
